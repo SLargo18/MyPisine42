7163683e3d1d428e7545fcf389ee7f0c42656f5f
Santiago Largo Botero <salargo-@c3r14s5.42madrid.com>
2025-09-29 15:38:06 +0200

    piscine

---
+++ piscine2/c06/a/ex03/ft_sort_params.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salargo- <salargo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 08:00:43 by salargo-          #+#    #+#             */
+/*   Updated: 2025/09/05 01:57:46 by salargo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_str_tab(char **tab, int size)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+		{
+			temp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		ft_sort_str_tab(argv + 1, argc - 1);
+		i = 1;
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
